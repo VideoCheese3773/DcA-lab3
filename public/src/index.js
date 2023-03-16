1585683421ef5168d19345ae47fe900fee6a5433
VideoCheese3773 <eedduxx@gmail.com>
2023-03-16 09:49:31 -0500

    new images

--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,28 @@
 //main index
+import * as modules from "./components/index.js";
+//import data from "./data.js";
+class AppContainter extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+
+    attributeChangedCallback(propname, oldvalue, newvalue) {
+        this[propname] = newvalue;
+        this.render();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="./src/index.css">
+
+        <p>bruh</p>
+        `
+    }
+}
+
+customElements.define("app-container", AppContainter);
